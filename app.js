397dc4b4c34f218c09a1ddf700225f60db67c8b8
Rajkumar saw <154462829+rajkumar963@users.noreply.github.com>
2024-01-19 18:02:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let doctor=document.querySelectorAll(".team");
+let count=0;
+doctor.forEach(function(card, index){
+    card.style.left=`${index*100}%`
+})
+function myFun(){
+   doctor.forEach(function(curValue){
+    curValue.style.transform=`translateX(-${count*100}%)`
+   })
+}
+setInterval(function(){
+    count++;
+    if(count==doctor.length){
+        count=0;
+    }
+    myFun()
+},3000)
